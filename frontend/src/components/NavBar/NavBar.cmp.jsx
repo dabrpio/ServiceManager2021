a2ae30ed160086b7a5262ae91c162730ede10127
piotrwrzodak <piotrwrzodakk@gmail.com>
2021-04-20 21:01:15 +0200

    change colors of selected link and links on hover & refactor navbar

--- frontend/src/components/NavBar/NavBar.cmp.jsx
+++ frontend/src/components/NavBar/NavBar.cmp.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {
   BsPlusSquareFill,
   BsFillPersonLinesFill,
@@ -9,34 +9,38 @@ import {
 import styles from './NavBar.module.scss';
 
 function NavBar() {
+  const NavBarLink = (props) => {
+    return (
+      <li className={styles.list__item}>
+        <NavLink
+          exact
+          to={props.to}
+          className={styles.list__item__link}
+          activeStyle={{ backgroundColor: '#e71d36' }}
+        >
+          {props.children}
+        </NavLink>
+      </li>
+    );
+  };
   return (
     <div className={styles.navbar}>
       <ul className={styles.list}>
-        <li className={styles.list__item}>
-          <Link to="/" className={styles.list__item__link}>
-            <BsFillHouseFill />
-          </Link>
-        </li>
-        <li className={styles.list__item}>
-          <Link to="/tickets" className={styles.list__item__link}>
-            <BsCardChecklist />
-          </Link>
-        </li>
-        <li className={styles.list__item}>
-          <Link to="/tickets/new" className={styles.list__item__link}>
-            <BsPlusSquareFill />
-          </Link>
-        </li>
-        <li className={styles.list__item}>
-          <Link to="/employees" className={styles.list__item__link}>
-            <BsFillPersonLinesFill />
-          </Link>
-        </li>
-        <li className={styles.list__item}>
-          <Link to="/settings" className={styles.list__item__link}>
-            <BsFillGearFill />
-          </Link>
-        </li>
+        <NavBarLink to="/">
+          <BsFillHouseFill />
+        </NavBarLink>
+        <NavBarLink to="/tickets">
+          <BsCardChecklist />
+        </NavBarLink>
+        <NavBarLink to="/tickets/new">
+          <BsPlusSquareFill />
+        </NavBarLink>
+        <NavBarLink to="/employees">
+          <BsFillPersonLinesFill />
+        </NavBarLink>
+        <NavBarLink to="/settings">
+          <BsFillGearFill />
+        </NavBarLink>
       </ul>
     </div>
   );
