ec1eaffa228f545a9cf3230efb9e7e5e9930fc71
piotrwrzodak <piotrwrzodakk@gmail.com>
2021-04-20 13:05:55 +0200

    improve login

--- frontend/src/components/Login/Login.cmp.jsx
+++ frontend/src/components/Login/Login.cmp.jsx
@@ -3,19 +3,29 @@ import { useState } from 'react';
 import styles from './Login.module.scss';
 
 function Login() {
+  const [nameValue, setNameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
   const [nameError, setNameError] = useState('');
   const [passwordError, setPasswordError] = useState('');
 
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    console.log(nameValue, passwordValue);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.login}>
+      <form className={styles.login} onSubmit={handleLoginSubmit}>
         <h1 className={styles.login__heading}>Service Manager 2021</h1>
         <input
           className={classnames(styles.login__input, {
             [styles.error]: nameError,
           })}
           type="text"
+          value={nameValue}
+          onChange={(event) => setNameValue(event.target.value)}
           placeholder="Nazwa"
+          required
         />
         <p className={styles.error_message}>{nameError}</p>
         <input
@@ -23,11 +33,18 @@ function Login() {
             [styles.error]: passwordError,
           })}
           type="password"
+          value={passwordValue}
+          onChange={(event) => setPasswordValue(event.target.value)}
           placeholder="Hasło"
+          required
         />
         <p className={styles.error_message}>{passwordError}</p>
-        <button className={styles.login__button}>Zaloguj</button>
-      </div>
+        <input
+          className={styles.login__button}
+          type="submit"
+          value="Zaloguj"
+        ></input>
+      </form>
     </div>
   );
 }
