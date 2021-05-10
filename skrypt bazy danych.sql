2ae768e72a9150bd3f15e9fa50b8c68ea29901dc
dlamparski <48011645+dlamparski@users.noreply.github.com>
2021-05-10 13:12:35 +0200

    Zmiana typów danych, dodano tabele użytkownicy

--- skrypt bazy danych.sql
+++ skrypt bazy danych.sql
@@ -1,17 +1,45 @@
-CREATE TABLE [dbo].[zlecenia] (
-    [RMA]            SMALLINT    IDENTITY,
-    [imie]           NCHAR (20)  NULL,
-    [nazwisko]       NCHAR (30)  NULL,
-    [nr_tel]         INT         NULL,
-    [data_przyjecia] DATETIME        default getdate(),
-    [rodzaj]         NCHAR (20)  NULL,
-    [marka]          NCHAR (20)  NULL,
-    [model]          NCHAR (20)  NULL,
-    [usterka]        NCHAR (300) NULL,
-    [status]         NCHAR (20)  NULL,
-    [koszt_naprawy]  MONEY       NULL,
-    [koszt_czesci]   MONEY       NULL,
-    [data_wydania]   DATE	 NULL,
-    [informacje]     NCHAR (100) NULL,
-    CONSTRAINT [PK_zlecenia] PRIMARY KEY CLUSTERED ([RMA] ASC)
-);
+CREATE TABLE [dbo].[zlecenia](
+	[RMA] [smallint] IDENTITY(1,1) NOT NULL,
+	[id_klienta] [smallint] NOT NULL,
+	[data_przyjecia] [datetime] NULL,
+	[rodzaj] [nvarchar](20) NOT NULL,
+	[marka] [nvarchar](20) NOT NULL,
+	[model] [nvarchar](20) NOT NULL,
+	[usterka] [nvarchar](300) NOT NULL,
+	[koszt_naprawy] [money] NOT NULL,
+	[koszt_czesci] [money] NULL,
+	[data_wydania] [date] NULL,
+	[status] [nvarchar](20) NOT NULL,
+	[informacje] [nvarchar](100) NULL,
+ CONSTRAINT [PK_zlecenia] PRIMARY KEY CLUSTERED 
+(
+	[RMA] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[zlecenia] ADD  DEFAULT (getdate()) FOR [data_przyjecia]
+GO
+
+ALTER TABLE [dbo].[zlecenia]  WITH CHECK ADD FOREIGN KEY([id_klienta])
+REFERENCES [dbo].[klienci] ([id_klienta])
+GO
+
+
+
+Klienci:
+
+CREATE TABLE [dbo].[klienci](
+	[id_klienta] [smallint] IDENTITY(1,1) NOT NULL,
+	[imie] [nvarchar](20) NOT NULL,
+	[nazwisko] [nvarchar](30) NOT NULL,
+	[nr_tel] [int] NULL,
+	[nazwa] [nvarchar](50) NULL,
+	[NIP] [int] NULL,
+	[e-mail] [nvarchar](100) NULL,
+ CONSTRAINT [PK_klienci] PRIMARY KEY CLUSTERED 
+(
+	[id_klienta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
