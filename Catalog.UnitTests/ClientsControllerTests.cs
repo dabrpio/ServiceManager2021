fd4c98d4ac1194677f3eeba32a00a057fef0faa4
dlamparski <lamparski.dominik@gmail.com>
2021-12-15 21:21:21 +0100

    ClientsControlletTests

---
+++ Catalog.UnitTests/ClientsControllerTests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommandApi.Controllers;
+using CommandApi.Dtos;
+using CommandApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using CommandApi.Models;
+using AutoMapper;
+using FluentAssertions;
+
+namespace Catalog.UnitTests;
+
+    public class ControllerTests
+    {
+        private readonly Mock<IClientsRepo> repositoryStub = new();
+        private readonly Mock<ILogger<ClientsController>> loggerStub = new();
+        private readonly Random rand = new();
+
+        private static IMapper _mapper;
+        
+        public ClientControllerTests()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new CommandApi.Profiles.DevicesProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+
+        [Fact]
+        public async Task GetDeviceById_WithUnexistingItem_ReturnsNotFound()
+        {
+            // Arrange
+            repositoryStub.Setup(repo => repo.GetDeviceById(It.IsAny<int>())).Returns((Device)null);
+
+            var controller = new DevicesController(repositoryStub.Object, _mapper);
+
+            // Act
+            var result =  controller.GetDeviceById(It.IsAny<int>());
+
+            // Assert
+            result.Result.Should().BeOfType<NotFoundResult>();
+
+
+
+        }
+
+        
+    }
+
+
+
+
+
